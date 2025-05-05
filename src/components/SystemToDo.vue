20cc2c203e672ee3692590500c140afc70f8ef54
MuhammadAidil24 <mhdaidiel24@gmail.com>
2025-05-05 22:58:56 +0700

    fifth commit : modifikasi template html dan menambah kan style pada style.css

--- src/components/SystemToDo.vue
+++ src/components/SystemToDo.vue
@@ -1,12 +1,29 @@
 <script setup>
-import { ref } from "vue";
-import { computed } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 
 let id = 0;
 
 const newTodo = ref("");
 const hideCompleted = ref(false);
 const todos = ref([]);
+
+// Muat data dari localStorage saat komponen di-mount
+onMounted(() => {
+  const savedTodos = localStorage.getItem("todos");
+  if (savedTodos) {
+    todos.value = JSON.parse(savedTodos);
+    id = todos.value.length ? Math.max(...todos.value.map((t) => t.id)) + 1 : 0;
+  }
+});
+
+// Simpan data ke localStorage setiap kali todos berubah
+watch(
+  todos,
+  (newTodos) => {
+    localStorage.setItem("todos", JSON.stringify(newTodos));
+  },
+  { deep: true }
+);
 
 const filteredTodos = computed(() => {
   return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
@@ -25,38 +42,31 @@ function removeTodo(todo) {
 </script>
 
 <template>
-  <div>
-    <h3>Input Aktivitas :</h3>
-  </div>
-
   <div class="input">
     <form @submit.prevent="addTodo">
+      <h3>Tambah Aktivitas</h3>
       <input v-model="newTodo" required placeholder="Tambahkan Tugas" />
       <button>Tambahkan</button>
     </form>
   </div>
 
   <div class="activity">
-    <h3>Activity</h3>
-  </div>
-
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done" />
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-
-  <div class="hideCompleted">
-    <button @click="hideCompleted = !hideCompleted">
-      {{ hideCompleted ? "Tampilkan Semua" : "Tugas Yang Belum Selesai" }}
-    </button>
+    <h3>Aktivitas</h3>
+    <div class="todoList">
+      <ul>
+        <li v-for="todo in filteredTodos" :key="todo.id">
+          <input type="checkbox" v-model="todo.done" />
+          <span :class="{ done: todo.done }">{{ todo.text }}</span>
+          <button @click="removeTodo(todo)">hapus</button>
+        </li>
+      </ul>
+    </div>
+    <div class="hideCompleted">
+      <button @click="hideCompleted = !hideCompleted">
+        {{ hideCompleted ? "Tampilkan Semua" : "Tugas Yang Belum Selesai" }}
+      </button>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.done {
-  text-decoration: line-through;
-}
-</style>
+<style scoped></style>
